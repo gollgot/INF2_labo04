c428b1f7c4fda17a26ec7d10b4e47bb54166989c
Loïc Dessaules <loic.dessaules@gmail.com>
2019-04-02 16:56:03 +0200

    Init project

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : Labo04 - Vecteur
+Fichier     : main.cpp
+Auteur(s)   : Loic Dessaules, Rosalie Chhen
+Date        : -
+
+But         : -
+
+Remarque(s) : -
+
+Compilateur : g++ <8.2.1>
+-----------------------------------------------------------------------------------
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "bla" << endl;
+
+    return 0;
+}
