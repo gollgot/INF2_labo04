15724972f3028f729947893af8a6656f7f734980
Unknown <chhen.rosalie@gmail.com>
2019-04-04 15:37:14 +0200

    add at method

--- VecteurImpl.h
+++ VecteurImpl.h
@@ -14,4 +14,17 @@ Vecteur<T>::Vecteur(size_t n) noexcept : data(std::vector<T>(n)) {}
 template <typename T>
 Vecteur<T>::Vecteur(const std::vector<T>& vector) noexcept : data(vector) {}
 
+
+template <typename T>
+T& Vecteur<T>::at(size_t n){
+    try {
+    data.at(n);
+    }catch(const out_of_range& e){
+        throw vecteur_out_of_range();
+    }
+}
+
+
+
+
 #endif //VECTEURIMPL_H
