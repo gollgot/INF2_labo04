e18d8e3b1b20c6284a9a5b05e4184e35261ffa0e
Loïc Dessaules <loic.dessaules@gmail.com>
2019-04-04 14:51:20 +0200

    Create Matrice skeleton

---
+++ Matrice.h
@@ -0,0 +1,8 @@
+//
+// Created by loic on 4/4/19.
+//
+
+#ifndef MATRICE_H
+#define MATRICE_H
+
+#endif //MATRICE_H
---
+++ MatriceImpl.h
@@ -0,0 +1,8 @@
+//
+// Created by loic on 4/4/19.
+//
+
+#ifndef MATRICEIMPL_H
+#define MATRICEIMPL_H
+
+#endif //MATRICEIMPL_H
